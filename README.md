ff2f165a2436ba69454afa9ab722929679eb99ed
Leon Shalimov <virtuexru@gmail.com>
2017-02-10 10:39:25 -0500

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# btc-price
-Show latest BTC price
+BTC Price
+=========
+
+A tiny library that fetches latest BTC price.
+
+## Installation
+
+  `npm install @soviet.pepe/btc-price`
+
+## Usage
+
+  In your terminal type: `btc-price`
+
+
+## Tests
+
+  None yet :)
+
+## Contributing
+
+  Go nuts; open a PR or Issue.
+  
---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+var http = require('http');
+
+http.get({
+    host: 'api.coindesk.com',
+    path: '/v1/bpi/currentprice.json'
+  },
+  function(response) {
+    var body = '';
+    response.on('data', function(d) { body += d; });
+    response.on('end', function() {
+      // console.log(JSON.parse(body));
+      console.log("Latest BTC price: " + JSON.parse(body).bpi.USD.rate);
+    });
+  }
+);
