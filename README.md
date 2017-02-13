c27b619e181b2b5e8e932d311fa53510493fe9c7
Leon Shalimov <virtuexru@gmail.com>
2017-02-13 11:54:09 -0500

    color and currency options

--- README.md
+++ README.md
@@ -11,12 +11,18 @@ A tiny library that fetches latest BTC price.
 
   In your terminal type: `btc-price`
 
+##### Options
 
-## Tests
+ - `-g` Display price in Great British Pounds
+ - `-e` Display price in Euro's
 
-  None yet :)
-
-## Contributing
+##### Contributing
 
   Go nuts; open a PR or Issue.
+
+##### Verion History  
+
+ - `0.1.0` Add color, error handling & currency options
+ - `0.0.2` Tagging & minor error fixes
+ - `0.0.1` Initial release
   
--- index.js
+++ index.js
@@ -1,16 +1,51 @@
 #!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
 var http = require('http');
+var program = require('commander');
+var chalk = require('chalk');
+
+/**
+ * Main
+ */
+program
+  .version('0.0.3')
+  .option('-g, --great-british-pound', 'Show price in Great British Pound')
+  .option('-e, --euro', 'Show price in Euro')
+  .parse(process.argv);
 
 http.get({
     host: 'api.coindesk.com',
     path: '/v1/bpi/currentprice.json'
   },
   function(response) {
+    var currency;
+    var symbol;
+
+    if(program.euro) {
+      currency = 'EUR';
+      symbol = '€';
+    } else if(program.greatBritishPound) {
+      currency = 'GBP';
+      symbol = '£';
+    } else {
+      currency = 'USD';
+      symbol = '$';
+    }
+
     var body = '';
     response.on('data', function(d) { body += d; });
     response.on('end', function() {
-      // console.log(JSON.parse(body));
-      console.log("Latest BTC price: $" + JSON.parse(body).bpi.USD.rate);
+      if (response.statusCode == 200) {
+        console.log("Latest BTC price: " + 
+          chalk.bgGreen.bold(symbol) +
+          chalk.bgGreen.bold(JSON.parse(body).bpi[currency].rate)
+        );
+      } else {
+        console.log(chalk.red("Error retrieving prices from api.coindesk.com :("));
+      }
     });
   }
 );
